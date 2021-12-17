29f08defc9fa0ecd781716871d808b64776b3e56
Steffen Kampmann <steffen.kampmann@gmail.com>
2021-12-17 11:18:11 +0100

    added import model tests

---
+++ test/Import-Model.Tests.ps1
@@ -0,0 +1,17 @@
+Describe 'Import-DacModel' {
+
+    BeforeDiscovery {
+        [System.IO.FileInfo] $Script:DacPacFile = "$PsScriptRoot\sql-server-samples\samples\databases\wide-world-importers\wwi-ssdt\wwi-ssdt\bin\Debug\WideWorldImporters.dacpac"
+    }
+
+    BeforeAll {
+        Import-Module $PSScriptRoot\..\src\PsDac\bin\Debug\net5.0\publish\PsDac.psd1 -ErrorAction Stop
+    }
+
+    Context 'DacPac' -Skip:( -Not $Script:DacPacFile.Exists ) {
+        It 'Imports the model from a DacPac' {
+            $model = Import-DacModel -Path $Script:DacPacFile
+            $model | Should -Not -BeNullOrEmpty
+        }
+    }
+}
