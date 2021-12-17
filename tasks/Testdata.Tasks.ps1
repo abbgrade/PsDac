73a7ab39451b5980eab9d96bda5cacbe107d9b32
Steffen Kampmann <steffen.kampmann@gmail.com>
2021-12-17 16:22:57 +0100

    updated tasks

--- tasks/Testdata.Tasks.ps1
+++ tasks/Testdata.Tasks.ps1
@@ -1,25 +1,16 @@
-[System.IO.FileInfo] $Script:TestdataBackup = "$PSScriptRoot\..\test\WideWorldImporters-Full.bak"
+# [System.IO.FileInfo] $Script:TestdataBackup = "$PSScriptRoot\..\test\WideWorldImporters-Full.bak"
 
-task Testdata.Download -If { -Not ( $Script:TestdataBackup.Exists ) } -Jobs {
-    Invoke-WebRequest `
-        -Uri https://github.com/microsoft/sql-server-samples/releases/download/wide-world-importers-v1.0/WideWorldImporters-Full.bak `
-        -OutFile $Script:TestdataBackup
-}
+# task Testdata.Download -If { -Not ( $Script:TestdataBackup.Exists ) } -Jobs {
+#     Invoke-WebRequest `
+#         -Uri https://github.com/microsoft/sql-server-samples/releases/download/wide-world-importers-v1.0/WideWorldImporters-Full.bak `
+#         -OutFile $Script:TestdataBackup
+# }
 
-task Testdata.ConnectDatabase -Jobs {
-    Import-Module PsSqlClient
-    Connect-TSqlInstance -DataSource '(LocalDb)\MSSQLLocalDB'
-}
-
-task Testdata.Restore -Jobs Testdata.Download, Testdata.ConnectDatabase, {
-
-}
-
-task Testdata.DacPac.InitSolution -Jobs {
+task Testdata.DacPac.WWI.Clean -If ( Test-Path $PSScriptRoot\..\test\sql-server-samples ) -Jobs {
     Remove-Item $PSScriptRoot\..\test\sql-server-samples -Recurse -Force
 }
 
-task Testdata.DacPac.CheckoutSolution -Jobs Testdata.DacPac.InitSolution, {
+task Testdata.DacPac.WWI.CheckoutSolution -If ( -Not ( Test-Path $PSScriptRoot\..\test\sql-server-samples )) -Jobs {
     $testdata = New-Item $PSScriptRoot\..\test\sql-server-samples -ItemType Directory
     Push-Location $testdata
     git init
@@ -30,15 +21,23 @@ task Testdata.DacPac.CheckoutSolution -Jobs Testdata.DacPac.InitSolution, {
     Pop-Location
 }
 
-task Testdata.DacPac.Create -Jobs {
+task Testdata.DacPac.WWI.Create -Jobs Testdata.DacPac.WWI.CheckoutSolution, {
     # # can be enabled if dotnet core build is public and working
     # exec { dotnet build "$PsScriptRoot\..\test\sql-server-samples\samples\databases\wide-world-importers\wwi-ssdt\wwi-ssdt\WideWorldImporters.sqlproj" /p:NetCoreBuild=true }
 
-    Install-Module Invoke-MsBuild
     Import-Module Invoke-MsBuild
+
     Invoke-MsBuild "$PsScriptRoot\..\test\sql-server-samples\samples\databases\wide-world-importers\wwi-ssdt\wwi-ssdt\WideWorldImporters.sqlproj"
 
     assert ( Test-Path "$PsScriptRoot\..\test\sql-server-samples\samples\databases\wide-world-importers\wwi-ssdt\wwi-ssdt\bin\Debug\WideWorldImporters.dacpac" )
 }
 
-task Testdata.Create -Jobs Testdata.DacPac.Create
+task Testdata.DacPac.TestDb.Create -Jobs {
+    Import-Module Invoke-MsBuild
+
+    Invoke-MsBuild "$PsScriptRoot\..\test\testdb\testdb.sqlproj"
+
+    assert ( Test-Path "$PsScriptRoot\..\test\testdb\bin\Debug\testdb.dacpac" )
+}
+
+task Testdata.Create -Jobs Testdata.DacPac.WWI.Create, Testdata.DacPac.TestDb.Create
