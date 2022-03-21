0a7c9d992534106f95e2f45cd64e3710cb87f8dd
Steffen Kampmann <steffen.kampmann@gmail.com>
2022-03-21 17:59:13 +0100

    added missing dependency

--- tasks/Dependency.Tasks.ps1
+++ tasks/Dependency.Tasks.ps1
@@ -5,6 +5,7 @@ task InstallBuildDependencies -Jobs {
 task InstallTestDependencies -Jobs {
     Install-Module PsSqlLocalDb -Scope CurrentUser -ErrorAction Stop -Verbose -AllowPrerelease
     Install-Module PsSqlTestServer -Scope CurrentUser -ErrorAction Stop -Verbose -AllowPrerelease
+    Install-Module PsSqlClient -Scope CurrentUser -ErrorAction Stop -Verbose -MaximumVersion 0.4.0
 }
 
 task InstallReleaseDependencies -Jobs {}
