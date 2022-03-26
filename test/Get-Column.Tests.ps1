4826937285e68d45d5bf3807e0aaee6979981129
Steffen Kampmann <steffen.kampmann@gmail.com>
2022-03-26 19:36:08 +0100

    changed variable scope

--- test/Get-Column.Tests.ps1
+++ test/Get-Column.Tests.ps1
@@ -3,17 +3,17 @@
 Describe 'Get-DacColumn' {
 
     BeforeDiscovery {
-        [System.IO.FileInfo] $Script:DacPacFile = "$PsScriptRoot\sql-server-samples\samples\databases\wide-world-importers\wwi-ssdt\wwi-ssdt\bin\Debug\WideWorldImporters.dacpac"
+        [System.IO.FileInfo] $Global:DacPacFile = "$PsScriptRoot\sql-server-samples\samples\databases\wide-world-importers\wwi-ssdt\wwi-ssdt\bin\Debug\WideWorldImporters.dacpac"
     }
 
     BeforeAll {
         Import-Module $PSScriptRoot\..\publish\PsDac\PsDac.psd1 -ErrorAction Stop
     }
 
-    Context 'DacPac' -Skip:( -Not $Script:DacPacFile.Exists ) {
+    Context 'DacPac' -Skip:( -Not $Global:DacPacFile.Exists ) {
         Context 'Model' {
             BeforeAll {
-                $Script:Model = Import-DacModel -Path $Script:DacPacFile
+                $Script:Model = Import-DacModel -Path $Global:DacPacFile
             }
             Context 'Table' {
                 BeforeAll {
