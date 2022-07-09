cf9c45365e8b9f896c83a6781d848aaa0e3bc08a
Steffen Kampmann <steffen.kampmann@gmail.com>
2022-07-09 13:07:01 +0200

    fixed variables

--- test/Install-Package.Tests.ps1
+++ test/Install-Package.Tests.ps1
@@ -1,16 +1,18 @@
 #Requires -Modules @{ ModuleName='Pester'; ModuleVersion='5.0.0' }, @{ ModuleName='PsSqlClient'; ModuleVersion='1.2.0' }, @{ ModuleName='PsSmo'; ModuleVersion='0.4.0' }, @{ ModuleName='PsSqlTestServer'; ModuleVersion='1.2.0' }
 
+param (
+    [System.IO.FileInfo] $TestDbDacPacFile = "$PsScriptRoot\testdb\bin\Debug\testdb.dacpac",
+    [System.IO.FileInfo] $WwiDacPacFile = "$PsScriptRoot\sql-server-samples\samples\databases\wide-world-importers\wwi-ssdt\wwi-ssdt\bin\Debug\WideWorldImporters.dacpac"
+)
+
 Describe 'Install-DacPackage' {
 
     BeforeDiscovery {
-        [System.IO.FileInfo] $Script:TestDbDacPacFile = "$PsScriptRoot\testdb\bin\Debug\testdb.dacpac"
-        [System.IO.FileInfo] $Script:WwiDacPacFile = "$PsScriptRoot\sql-server-samples\samples\databases\wide-world-importers\wwi-ssdt\wwi-ssdt\bin\Debug\WideWorldImporters.dacpac"
-
-        if ( -Not $Script:TestDbDacPacFile.Exists ) {
+        if ( -Not $TestDbDacPacFile.Exists ) {
             Write-Warning "Skip tests with testdb.dacpac requirement."
         }
 
-        if ( -Not $Script:WwiDacPacFile.Exists ) {
+        if ( -Not $WwiDacPacFile.Exists ) {
             Write-Warning "Skip tests with WideWorldImporters.dacpac requirement."
         }
     }
@@ -22,22 +24,22 @@ Describe 'Install-DacPackage' {
     Context 'Server' {
 
         BeforeAll {
-            $Script:TestServer = New-SqlTestInstance
-            $Script:SqlConnection = Connect-TSqlInstance -ConnectionString $Script:TestServer.ConnectionString
+            $TestServer = New-SqlTestInstance
+            $SqlConnection = Connect-TSqlInstance -ConnectionString $TestServer.ConnectionString
         }
 
         AfterAll {
-            $Script:TestServer | Remove-SqlTestInstance
+            $TestServer | Remove-SqlTestInstance
         }
 
         Context 'DacService' {
 
             BeforeAll {
-                $Script:DacService = Connect-DacService -ConnectionString $Script:TestServer.ConnectionString -ErrorAction Stop
+                $DacService = Connect-DacService -ConnectionString $TestServer.ConnectionString -ErrorAction Stop
             }
 
             AfterAll {
-                if ( $Script:DacService ) {
+                if ( $DacService ) {
                     Disconnect-DacService
                 }
             }
@@ -45,46 +47,46 @@ Describe 'Install-DacPackage' {
             Context 'Database' {
 
                 BeforeEach {
-                    [string] $Script:DatabaseName = ( [string](New-Guid) ).Substring(0, 8)
+                    [string] $DatabaseName = ( [string](New-Guid) ).Substring(0, 8)
                 }
 
                 AfterEach {
-                    Invoke-TSqlCommand "DROP DATABASE [$Script:DatabaseName];"
+                    Invoke-TSqlCommand "DROP DATABASE [$DatabaseName];"
                 }
 
-                Context 'testdb DacPac' -Skip:( -Not $Script:TestDbDacPacFile.Exists ) {
+                Context 'testdb DacPac' -Skip:( -Not $TestDbDacPacFile.Exists ) {
 
                     BeforeAll {
-                        $Script:DacPac = Import-DacPackage $Script:TestDbDacPacFile
+                        $DacPac = Import-DacPackage -Path $TestDbDacPacFile.FullName
                     }
 
                     It 'Creates database objects of a dacpac' {
-                        Install-DacPackage $Script:DacPac -DatabaseName $Script:DatabaseName
+                        Install-DacPackage $DacPac -DatabaseName $DatabaseName
 
-                        Invoke-TSqlCommand "USE [$Script:DatabaseName]" -Connection $Script:SqlConnection
-                        $Script:SmoConnection = Connect-SmoInstance -Connection $Script:SqlConnection
+                        Invoke-TSqlCommand "USE [$DatabaseName]" -Connection $SqlConnection
+                        $SmoConnection = Connect-SmoInstance -Connection $SqlConnection
 
                         Get-SmoTable -Name MyTable | Should -Not -BeNullOrEmpty
 
-                        Disconnect-SmoInstance -Instance $Script:SmoConnection
+                        Disconnect-SmoInstance -Instance $SmoConnection
                     }
                 }
 
-                Context 'wwi DacPac' -Skip:( -Not $Script:WwiDacPacFile.Exists ) {
+                Context 'wwi DacPac' -Skip:( -Not $WwiDacPacFile.Exists ) {
 
                     BeforeAll {
-                        $Script:DacPac = Import-DacPackage -Path $Script:WwiDacPacFile
+                        $DacPac = Import-DacPackage -Path $WwiDacPacFile.FullName
                     }
 
                     It 'Creates database objects of a dacpac' {
-                        Install-DacPackage $Script:DacPac -DatabaseName $Script:DatabaseName #-ExcludeObjectTypes Logins, Files, Filegroups -Verbose
+                        Install-DacPackage $DacPac -DatabaseName $DatabaseName #-ExcludeObjectTypes Logins, Files, Filegroups -Verbose
 
-                        Invoke-TSqlCommand "USE [$Script:DatabaseName]" -Connection $Script:SqlConnection
-                        $Script:SmoConnection = Connect-SmoInstance -Connection $Script:SqlConnection
+                        Invoke-TSqlCommand "USE [$DatabaseName]" -Connection $SqlConnection
+                        $SmoConnection = Connect-SmoInstance -Connection $SqlConnection
 
                         Get-SmoTable -Name MyTable | Should -Not -BeNullOrEmpty
 
-                        Disconnect-SmoInstance -Instance $Script:SmoConnection
+                        Disconnect-SmoInstance -Instance $SmoConnection
                     }
                 }
             }
