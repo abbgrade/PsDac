29ade341f1ea106d0cfd36dd6b67b79659e2aeb3
Steffen Kampmann <steffen.kampmann@gmail.com>
2022-07-30 21:25:55 +0200

    remove version dependency

--- tasks/Dependency.Tasks.ps1
+++ tasks/Dependency.Tasks.ps1
@@ -6,7 +6,7 @@ task InstallTestDependencies -Jobs {
     Install-Module Pester -MinimumVersion '5.0.0'
     Install-Module PsSqlLocalDb -Scope CurrentUser -ErrorAction Stop -Verbose -AllowPrerelease
     Install-Module PsSqlTestServer -Scope CurrentUser -ErrorAction Stop -Verbose -AllowPrerelease
-    Install-Module PsSqlClient -Scope CurrentUser -ErrorAction Stop -Verbose -MaximumVersion 0.4.0
+    Install-Module PsSqlClient -Scope CurrentUser -ErrorAction Stop -Verbose -AllowClobber
     Install-Module PsSmo -Scope CurrentUser -ErrorAction Stop -Verbose -AllowPrerelease
     Install-Module Invoke-MsBuild -ErrorAction Stop
 }, TestData.Create
