fb1356000ff0f21f4eb0a374910c6b34cb9d3e42
Steffen <skampmann@munichre.com>
2023-02-17 14:05:55 +0100

    added test

--- test/Import-Model.Tests.ps1
+++ test/Import-Model.Tests.ps1
@@ -1,4 +1,4 @@
-#Requires -Modules @{ ModuleName='Pester'; ModuleVersion='5.0.0' }
+#Requires -Modules @{ ModuleName='Pester'; ModuleVersion='5.0.0' }, @{ ModuleName='PsSqlTestServer'; ModuleVersion='1.2.0' }
 
 param (
     [System.IO.FileInfo] $DacPacFile = "$PsScriptRoot\sql-server-samples\samples\databases\wide-world-importers\wwi-ssdt\wwi-ssdt\bin\Debug\WideWorldImporters.dacpac"
@@ -14,6 +14,47 @@ Describe Import-Model {
         It 'Imports the model from a DacPac' {
             $Model = Import-DacModel -Path $DacPacFile
             $Model | Should -Not -BeNullOrEmpty
+        }
+    }
+
+
+    Context Server {
+
+        BeforeAll {
+            $TestServer = New-SqlTestInstance
+            $SqlConnection = Connect-TSqlInstance -ConnectionString $TestServer.ConnectionString
+        }
+
+        AfterAll {
+            $TestServer | Remove-SqlTestInstance
+        }
+
+        Context DacService {
+
+            BeforeAll {
+                $DacService = $TestServer | Connect-DacService -ErrorAction Stop
+            }
+
+            AfterAll {
+                if ( $DacService ) {
+                    Disconnect-DacService
+                }
+            }
+
+            Context Database {
+                BeforeAll {
+                    $Database = New-SqlTestDatabase -Instance $TestServer -InstanceConnection $SqlConnection
+                }
+
+                AfterAll {
+                    $Database | Remove-SqlTestDatabase
+                }
+
+                It 'Extracts from the database' {
+                    $Model = Import-DacModel -DatabaseName $Database.Name -ErrorAction Stop
+                    $Model | Should -Not -BeNullOrEmpty
+                }
+            }
         }
     }
 }
