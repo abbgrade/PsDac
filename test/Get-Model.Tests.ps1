2bfe630be609e7c8db9d547883b79baff1681e62
Steffen <skampmann@munichre.com>
2023-03-14 13:50:07 +0100

    fixed test

--- test/Get-Model.Tests.ps1
+++ test/Get-Model.Tests.ps1
@@ -1,22 +1,23 @@
 #Requires -Modules @{ ModuleName='Pester'; ModuleVersion='5.0.0' }
 
-Describe Get-DacModel -Tag NotImplemented {
+param (
+    [System.IO.FileInfo] $DacPacFile = "$PsScriptRoot\sql-server-samples\samples\databases\wide-world-importers\wwi-ssdt\wwi-ssdt\bin\Debug\WideWorldImporters.dacpac"
+)
 
-    BeforeDiscovery {
-        [System.IO.FileInfo] $Script:DacPacFile = "$PsScriptRoot\sql-server-samples\samples\databases\wide-world-importers\wwi-ssdt\wwi-ssdt\bin\Debug\WideWorldImporters.dacpac"
-    }
+Describe Get-DacModel -Tag NotImplemented {
 
     BeforeAll {
         Import-Module $PSScriptRoot\..\publish\PsDac\PsDac.psd1 -ErrorAction Stop
     }
 
-    Context 'Model' -Skip:( -Not $Script:DacPacFile.Exists ) {
+    Context 'Model' -Skip:( -Not $DacPacFile.Exists ) {
         BeforeAll {
-            $Script:DacPac = Import-DacModel $Script:DacPacFile
+            Write-Verbose $DacPacFile -Verbose
+            $DacPac = Import-DacModel -Path $DacPacFile -Verbose
         }
 
         It 'Returns the model of a package' {
-            $model = $Script:DacPac | Get-DacModel
+            $model = $DacPac | Get-DacModel
             $model | Should -Not -BeNullOrEmpty
         }
     }
