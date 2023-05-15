3c1c41152fc8872fc492e3288109a02165f274fb
Steffen Kampmann <steffen.kampmann@gmail.com>
2023-05-15 16:24:20 +0200

    added connect test cases

--- test/Connect-Service.Tests.ps1
+++ test/Connect-Service.Tests.ps1
@@ -4,6 +4,7 @@ Describe Connect-Service {
 
     BeforeAll {
         Import-Module $PSScriptRoot\..\publish\PsDac\PsDac.psd1 -ErrorAction Stop
+        $VerbosePreference = 'Continue'
     }
 
     Context TestDatabase {
@@ -31,6 +32,58 @@ Describe Connect-Service {
         It 'Creates a service by pipeline' {
             $Service = $TestServer | Connect-DacService -ErrorAction Stop
             $Service | Should -Not -BeNullOrEmpty
+        }
+    }
+
+    Context AzureSqlServer -Tag Azure {
+
+        BeforeAll {
+            $TestServer = New-SqlTestAzureInstance -Subscription 'Visual Studio' -ErrorAction Stop
+        }
+
+        AfterAll {
+            if ( $TestServer ) {
+                $TestServer | Remove-SqlTestAzureInstance
+            }
+        }
+
+        It 'Creates a service by datasource' {
+            $Service = Connect-DacService -DataSource $TestServer.DataSource -ErrorAction Stop
+            $Service | Should -Not -BeNullOrEmpty
+        }
+
+        It 'Creates a service by connection string' {
+            $Service = Connect-DacService -ConnectionString $TestServer.ConnectionString -ErrorAction Stop
+            $Service | Should -Not -BeNullOrEmpty
+        }
+
+        It 'Creates a service by pipeline' {
+            $Service = $TestServer | Connect-DacService -ErrorAction Stop
+            $Service | Should -Not -BeNullOrEmpty
+        }
+
+        Context AccessToken {
+
+            BeforeAll {
+                $TestServerWithToken = $TestServer.PsObject.Copy()
+                $TestServerWithToken | Add-Member AccessToken ( Get-AzAccessToken -ResourceUrl https://database.windows.net ).Token
+                $TestServerWithToken.ConnectionString += ";Access Token=" + $TestServer.AccessToken
+            }
+
+            It 'Creates a service by datasource' {
+                $Service = Connect-DacService -DataSource $TestServerWithToken.DataSource -AccessToken $TestServerWithToken.AccessToken -ErrorAction Stop
+                $Service | Should -Not -BeNullOrEmpty
+            }
+
+            It 'Creates a service by connection string' {
+                $Service = Connect-DacService -ConnectionString $TestServerWithToken.ConnectionString -ErrorAction Stop
+                $Service | Should -Not -BeNullOrEmpty
+            }
+
+            It 'Creates a service by pipeline' {
+                $Service = $TestServerWithToken | Connect-DacService -ErrorAction Stop
+                $Service | Should -Not -BeNullOrEmpty
+            }
         }
     }
 }
